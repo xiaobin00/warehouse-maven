711f2e2b4042b5b833863113a5c4ebe51cbc03f5
xiaobin00 <981585755@qq.com>
2016-10-18 22:59:03 +0800

    [实体类]

---
+++ warehouse-admin/src/main/java/light/mvc/model/basic/ProcedureDepartment.java
@@ -0,0 +1,108 @@
+package light.mvc.model.basic;
+
+import java.sql.Timestamp;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import static javax.persistence.GenerationType.IDENTITY;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * ProcedureDepartment entity. @author MyEclipse Persistence Tools
+ */
+@Entity
+@Table(name = "procedure_department", catalog = "bsale")
+public class ProcedureDepartment implements java.io.Serializable {
+
+	// Fields
+
+	private Integer id;
+	private String name;
+	private String charge;
+	private Integer companyId;
+	private Integer status;
+	private Timestamp createTime;
+
+	// Constructors
+
+	/** default constructor */
+	public ProcedureDepartment() {
+	}
+
+	/** minimal constructor */
+	public ProcedureDepartment(Integer companyId, Integer status,
+			Timestamp createTime) {
+		this.companyId = companyId;
+		this.status = status;
+		this.createTime = createTime;
+	}
+
+	/** full constructor */
+	public ProcedureDepartment(String name, String charge, Integer companyId,
+			Integer status, Timestamp createTime) {
+		this.name = name;
+		this.charge = charge;
+		this.companyId = companyId;
+		this.status = status;
+		this.createTime = createTime;
+	}
+
+	// Property accessors
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	@Column(name = "id", unique = true, nullable = false)
+	public Integer getId() {
+		return this.id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	@Column(name = "name", length = 50)
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Column(name = "charge", length = 50)
+	public String getCharge() {
+		return this.charge;
+	}
+
+	public void setCharge(String charge) {
+		this.charge = charge;
+	}
+
+	@Column(name = "company_id", nullable = false)
+	public Integer getCompanyId() {
+		return this.companyId;
+	}
+
+	public void setCompanyId(Integer companyId) {
+		this.companyId = companyId;
+	}
+
+	@Column(name = "status", nullable = false)
+	public Integer getStatus() {
+		return this.status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	@Column(name = "create_time", nullable = false, length = 19)
+	public Timestamp getCreateTime() {
+		return this.createTime;
+	}
+
+	public void setCreateTime(Timestamp createTime) {
+		this.createTime = createTime;
+	}
+
+}
